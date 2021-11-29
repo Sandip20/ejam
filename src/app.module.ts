5c41adc822187eb60810f604d69195db3b91daba
Sandip Pawar <sandip.pawar@globant.com>
2021-11-29 08:38:55 +0530

    test app

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { DomainsController } from './modules/domains/domains.controller';
+import { DomainsModule } from './modules/domains/domains.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from './config/config.service';
+
+@Module({
+  imports: [
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.getMongoConfig(),
+    }),
+    DomainsModule,
+    OrdersModule,
+  ],
+  controllers: [AppController, DomainsController],
+  providers: [AppService],
+})
+export class AppModule {}
